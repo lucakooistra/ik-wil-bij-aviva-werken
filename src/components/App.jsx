6a49bc4ebef35ba2ff08d53be8a171186a90810a
Luca Kooistra <luca.kooistra@avivasolutions.nl>
2023-03-20 22:46:55 +0100

    add navbar to project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,13 +3,11 @@ import Recipes from "./Recipes";
 import sword from "../images/swc-sword.png";
 import swordSvg from "../images/sword.svg";
 
+
 const App = () => {
     return (
         <>
             <main>
-                <section>
-                    <h1>Oh Hai, React</h1>
-                </section>
                 <img src={sword} alt="sword" width="250px"/>
                 <img src={swordSvg} alt="sword" width="250px"/>
                 <Recipes />
